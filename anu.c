a7ac540374e88dcf2c09af0778f63517af5f2426
KSAnushree <anushreetutu2006@gmai.com>
2024-08-20 20:39:31 +0530

    new commit

---
+++ anu.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+// sum of two numbers
+int main(){
+int a,b;
+printf("enter a =");
+scanf("%d",&a);
+
+printf("enter b =");
+scanf("%d",&b);
+
+int sum= a+b;
+printf("%d is sum", sum);
+
+//product of two numbers
+int c,d;
+printf("enter c=");
+scanf("%d",&c);
+
+printf("enter d");
+scanf("%d",&d);
+
+int pro=c*d;
+printf("pro is = %d", pro);
+
+return 0;
+}
