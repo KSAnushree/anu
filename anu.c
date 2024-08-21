fb25a3b461daa1a477d7a776efebaeeb324ae4e0
KSAnushree <anushreetutu2006@gmai.com>
2024-08-21 18:53:12 +0530

    commit2

--- anu.c
+++ anu.c
@@ -10,14 +10,14 @@ printf("enter b =");
 scanf("%d",&b);
 
 int sum= a+b;
-printf("%d is sum", sum);
+printf("%d is sum", sum n\);
 
 //product of two numbers
 int c,d;
 printf("enter c=");
 scanf("%d",&c);
 
-printf("enter d");
+printf("enter d =");
 scanf("%d",&d);
 
 int pro=c*d;
