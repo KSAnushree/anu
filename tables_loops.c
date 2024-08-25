5ac913a6e3ce24e9774f12dae0d990f215241740
KSAnushree <anushreetutu2006@gmai.com>
2024-08-25 22:52:37 +0530

    commit 4

--- tables_loops.c
+++ tables_loops.c
@@ -7,6 +7,11 @@ scanf("%d", &n);
 for (int i=1; i<=10;i++){
    printf("%d\n",n*i);
 }
+     printf("enter number:");
+     scanf("%d",&n);
 
+     for(int i=10;i>=1;i--){
+     printf("%d\n", n*i);
+     }
     return 0;
 }
