4d3d63188c68794aacd3e09993fdd870f3f3d2c3
KSAnushree <anushreetutu2006@gmai.com>
2024-08-29 19:33:32 +0530

    fuction_area

---
+++ function_area.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+float square_area(float side);
+float circle_area(float radius);
+float rectangle_area(float a, float b);
+
+int main (){
+   float a = 2.0;
+   float b = 2.0;
+  printf(" area is :%f\n", rectangle_area(a,b));
+
+  float side= 5;
+  printf("area is %f\n", square_area(side));
+
+  float radius= 2;
+  printf("area is %f", circle_area(radius));
+
+ return 0;
+}
+  
+  float square_area(float side){
+
+    return side*side;
+  }
+
+  float circle_area(float radius){
+
+    return 3.14*radius*radius;
+  }
+
+  float rectangle_area(float a , float b){
+
+    return a*b;
+  }
