5208b83e938b170bd376ff12ba6a3f974a0f69af
KSAnushree <anushreetutu2006@gmai.com>
2024-09-19 23:26:37 +0530

    problems

---
+++ prob_10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+// count number of digits.
+int main(){
+int n;
+printf("enter number:");
+scanf("%d" , &n);
+
+int i=0;
+while (n!=0){
+    n =n/10;
+    i++;
+}
+printf("The number of digits in an integer is : %d",i);  
+    return 0;
+}
